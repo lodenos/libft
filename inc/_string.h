61d9f1960a192f4cadcd852368125a01e0d1fd8e
lodenos <nikolaiilodenos@gmail.com>
2020-02-12 22:15:46 +0000

    _strlen is coded

--- inc/_string.h
+++ inc/_string.h
@@ -1,30 +1,28 @@
 #ifndef _STRING_H
 #define _STRING_H
 
-#include "_stdint.h"
-
 extern i32 _bcmp(void const *restrict const sourceA,
-                 void const *restrict const sourceB, isize size);
+                 void const *restrict const sourceB, usize size);
 extern void _bcopy(void const *restrict const source,
-                   void const *restrict destination, isize size);
-extern void _bzero(void const *restrict source, isize size);
-extern void _explicit_bzero(void const *restrict source, isize size);
+                   void const *restrict destination, usize size);
+extern void _bzero(void const *restrict source, usize size);
+extern void _explicit_bzero(void const *restrict source, usize size);
 extern u8 *_index(u8 const *restrict const source, i32 character);
 extern void *_memccpy(void const *restrict const source,
                       void const *restrict destination, i32 character,
-                      isize size);
+                      usize size);
 extern void *_memchr(void const *restrict const source, i32 character,
-                     isize size);
+                     usize size);
 extern i32 _memcmp(void const *restrict const sourceA,
-                   void const *restrict const sourceB, isize size);
+                   void const *restrict const sourceB, usize size);
 extern void *_memcpy(void const *restrict const source,
-                     void const *restrict destination, isize size);
+                     void const *restrict destination, usize size);
 extern void *_memmove(void const *restrict const source,
-                      void const *restrict destination, isize size);
+                      void const *restrict destination, usize size);
 extern void *_memrchr(void const *restrict const source, i32 character,
-                      isize size);
+                      usize size);
 extern void *_memset(void const *restrict const source, i32 character,
-                     isize size);
+                     usize size);
 extern void *_rawmemchr(void const *restrict const source, i32 character);
 extern u8 *_rindex(u8 const *restrict const source, i32 character);
 extern u8 *_strcasestr(const u8 *haystack, const u8 *needle);
@@ -38,15 +36,15 @@ extern u8 *_strcpy(u8 const *restrict const source,
                    u8 const *restrict const destination);
 extern u8 *_strdup(u8 const *restrict const source);
 extern u8 *_strdupa(u8 const *restrict const source);
-extern isize _strlen(u8 const *restrict const source);
+extern usize _strlen(u8 const *restrict const source);
 extern u8 *_strncat(u8 const *restrict const source,
-                    u8 const *restrict destination, isize size);
+                    u8 const *restrict destination, usize size);
 extern i32 _strncmp(u8 const *restrict const sourceA,
-                    u8 const *restrict const sourceB, isize size);
+                    u8 const *restrict const sourceB, usize size);
 extern u8 *_strncpy(u8 const *restrict const source,
-                    u8 const *restrict destination, isize size);
-extern u8 *_strndup(u8 const *restrict const source, isize size);
-extern u8 *_strndupa(u8 const *restrict const source, isize size);
+                    u8 const *restrict destination, usize size);
+extern u8 *_strndup(u8 const *restrict const source, usize size);
+extern u8 *_strndupa(u8 const *restrict const source, usize size);
 extern u8 *_strrchr(u8 const *restrict const source, u32 character);
 extern u8 *_strstr(const u8 *haystack, const u8 *needle);
 
--- src/_string/_strlen.c
+++ src/_string/_strlen.c
@@ -1,6 +1,35 @@
 #include "_string.h"
 
-isize _strlen(u8 const *restrict const source) {
-  (void)source;
-  return 0;
+static u64 const mask01 = 0x0101010101010101;
+static u64 const mask80 = 0x8080808080808080;
+
+usize _strlen(u8 const *restrict const source) {
+  register u8 const *restrict ptr8;
+  register u64 const *restrict ptr64;
+  for (ptr8 = source; (u64)ptr8 & 63; ++ptr8)
+    if (*ptr8 == 0)
+      return ptr8 - source;
+  ptr64 = (u64 *)ptr8;
+loop:
+  if ((*ptr64 - mask01) & mask80) {
+    ptr8 = (u8 *)ptr64;
+    if (ptr8[0] == 0)
+      return ptr8 - source;
+    if (ptr8[1] == 0)
+      return ptr8 - source + 1;
+    if (ptr8[2] == 0)
+      return ptr8 - source + 2;
+    if (ptr8[3] == 0)
+      return ptr8 - source + 3;
+    if (ptr8[4] == 0)
+      return ptr8 - source + 4;
+    if (ptr8[5] == 0)
+      return ptr8 - source + 5;
+    if (ptr8[6] == 0)
+      return ptr8 - source + 6;
+    if (ptr8[7] == 0)
+      return ptr8 - source + 7;
+  }
+  ++ptr64;
+  goto loop;
 }
