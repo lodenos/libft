c8f735b574dc14711c95c6d71d6f579aa5e9de4a
Nikolaii Lodenos <nikolaiilodenos@gmail.com>
2023-10-30 19:06:30 +0000

    add ft_atoi

---
+++ src/ft_stdlib/ft_atoi.c
@@ -0,0 +1,14 @@
+int ft_atoi(char const *str) {
+  int sign;
+  int value;
+
+  sign = 1;
+  value = 0;
+  while (*str == 0x20 || (*str > 0x08 && *str < 0x0E))
+    ++str;
+  if ((*str == 0x2B || *str == 0x2D) && (*str == 0x2D))
+    sign *= -1;
+  while (*str > 0x2F && *str < 0x3A)
+    value = value * 10 + *str++ - 0x30;
+  return value * sign;
+}
