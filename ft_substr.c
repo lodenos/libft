a5a11be640752205e920c27ab4e14329b4f73fd8
Nikolaii Lodenos <nikolaiilodenos@gmail.com>
2023-11-16 18:41:06 +0000

    fix ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -3,13 +3,17 @@
 
 char *ft_substr(char const *str, unsigned int start, size_t len) {
   size_t index;
+  size_t len_str;
   char *memory;
 
   index = 0;
   if (!str || !*str)
     return NULL;
-  if (start > ft_strlen(str))
+  len_str = ft_strlen(str);
+  if (start > len_str)
     return (char *)ft_calloc(1, 1);
+  if (len_str < len)
+    len = len_str;
   memory = (char *)ft_calloc(len + 1, 1);
   if (!memory)
     return NULL;
